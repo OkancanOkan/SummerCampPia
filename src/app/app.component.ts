80e8e7c33663818a9b5f36a7bfbe7e15ad1bc64a
Okancan Okan <okancan.okan@gmail.com>
2022-06-23 16:11:25 +0300

    addingDoctor getServicesAdded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {logMessages} from "@angular-devkit/build-angular/src/builders/browser-esbuild/esbuild";
-import {HospitalDataService} from "./services/hospital-data.service";
 
 @Component({
   selector: 'app-root',
@@ -9,11 +8,4 @@ import {HospitalDataService} from "./services/hospital-data.service";
 })
 export class AppComponent {
 title=""
-  users:any;
-  constructor(private hospitalData:HospitalDataService) {
-    this.hospitalData.users().subscribe((data)=>(
-       this.users=data
-      )
-    )
-  }
 }
