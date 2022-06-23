fca7a26bc8121af127f745bede9aff224997bd01
Okancan Okan <okancan.okan@gmail.com>
2022-06-23 17:06:18 +0300

    addingDoctor getServicesAdded

--- src/app/services/department-data.service.ts
+++ src/app/services/department-data.service.ts
@@ -9,7 +9,6 @@ export class DepartmentDataService {
 
   _url="http://172.16.88.63:8080/api/clinics/2"
 
-
   constructor(private http:HttpClient) { }
   departments(){
     return this.http.get(this._url)
